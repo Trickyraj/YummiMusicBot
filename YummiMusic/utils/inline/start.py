4ab99977035806994cd8d33a80c3caedb39b2428
Trickyraj <137736232+Trickyraj@users.noreply.github.com>
2023-07-08 16:29:47 +0530

    YummiMusic

--- YummiMusic/utils/inline/start.py
+++ YummiMusic/utils/inline/start.py
@@ -30,26 +30,26 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✦𝐀𝐝𝐝 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩✦",
+                text="ᴀᴅᴅ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="☆𝐇𝐞𝐥𝐩☆", callback_data="settings_back_helper"
+                text="ʜᴇʟᴘ", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="☆𝐆𝐫𝐨𝐮𝐩☆", url=config.SUPPORT_GROUP
+                text="ɢʀᴏᴜᴏ", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="☆𝐂𝐡𝐚𝐧𝐧𝐞𝐥☆", url=config.SUPPORT_CHANNEL
+                text="ᴄʜᴀɴɴᴇʟ", url=config.SUPPORT_CHANNEL
             )
         ],
         [
             InlineKeyboardButton(
-                text="✦𝐎𝐰𝐧𝐞𝐫✦", user_id=OWNER
+                text="ᴏᴡɴᴇʀ", user_id=OWNER
             )
         ],
      ]
