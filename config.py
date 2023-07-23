ccf9d458fb34cfac955c4345d7a6f62dd78497bf
Trickyraj <137736232+Trickyraj@users.noreply.github.com>
2023-07-23 07:33:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/irtkaal/YummiMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/YummiUpdate")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/YummiSupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/YippeeiUpdate")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Yippeeisupport")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
