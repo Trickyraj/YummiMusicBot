5522439a190b47325b94a7d23331e08c828e6e5d
Trickyraj <137736232+Trickyraj@users.noreply.github.com>
2023-08-21 21:13:48 +0530

    Yummi Music

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Yᴜᴍᴍɪ Mᴜsɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6209716882").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/irtkaal/YummiMusic")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Trickyraj/YummiMusicBot")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Trickyraj")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/YippeeiUpdate")
