04ab8e01281e67f74a871a9893ae114e7bf8a7b3
Trickyraj <137736232+Trickyraj@users.noreply.github.com>
2023-09-01 10:31:34 +0530

    Yummi Music

--- YummiMusic/plugins/autoleave.py
+++ YummiMusic/plugins/autoleave.py
@@ -1,45 +1,39 @@
 import asyncio
 from datetime import datetime
 
+from pyrogram.enums import ChatType
+
 import config
 from YummiMusic import app
 from YummiMusic.core.call import Yummi, autoend
-from YummiMusic.utils.database import (get_client, is_active_chat,
-                                       is_autoend)
+from YummiMusic.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT == str(True):
-        while not await asyncio.sleep(
-            config.AUTO_LEAVE_ASSISTANT_TIME
-        ):
+    if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(900):
             from YummiMusic.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
                 left = 0
                 try:
-                    async for i in client.iter_dialogs():
-                        chat_type = i.chat.type
-                        if chat_type in [
-                            "supergroup",
-                            "group",
-                            "channel",
+                    async for i in client.get_dialogs():
+                        if i.chat.type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
                         ]:
-                            chat_id = i.chat.id
                             if (
-                                chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001686672798
-                                and chat_id != -1001840101403
-                                and chat_id != -1001549206010
+                                i.chat.id != config.LOGGER_ID
+                                and i.chat.id != -1001686672798
+                                and i.chat.id != -1001549206010
                             ):
                                 if left == 20:
                                     continue
-                                if not await is_active_chat(chat_id):
+                                if not await is_active_chat(i.chat.id):
                                     try:
-                                        await client.leave_chat(
-                                            chat_id
-                                        )
+                                        await client.leave_chat(i.chat.id)
                                         left += 1
                                     except:
                                         continue
@@ -52,7 +46,8 @@ asyncio.create_task(auto_leave())
 
 async def auto_end():
     while not await asyncio.sleep(5):
-        if not await is_autoend():
+        ender = await is_autoend()
+        if not ender:
             continue
         for chat_id in autoend:
             timer = autoend.get(chat_id)
@@ -77,3 +72,4 @@ async def auto_end():
 
 
 asyncio.create_task(auto_end())
+. 
